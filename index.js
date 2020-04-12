7f66620e467f1fce1568d7e7c02b9e9ef8345d8f
viveksingh-01 <viveksingh9301@gmail.com>
2020-04-12 23:16:03 +0530

    Add routing file for authentication

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const app = express();
 
+const router = express.Router();
+
+const authRoute = require('./routes/auth');
+app.use('/api/user', authRoute);
+
 const port = 3000;
 app.listen(port, () => {
   console.log('Server up and running...');
---
+++ routes/auth.js
@@ -0,0 +1,7 @@
+const router = require('express').Router();
+
+router.get('/register', (req, res) => {
+  res.send('register');
+});
+
+module.exports = router;
