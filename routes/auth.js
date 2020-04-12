80783cf3ecbbe5976d45a749a593f71bc697a1a9
viveksingh-01 <viveksingh9301@gmail.com>
2020-04-13 02:27:23 +0530

    Save user data on successful registration

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,21 @@
 const router = require('express').Router();
 
-router.get('/register', (req, res) => {
-  res.send('register');
+// Import User Model
+const User = require('../models/User');
+
+router.post('/register', async (req, res) => {
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  try {
+    const savedUser = await user.save();
+    res.send(savedUser);
+  } catch (err) {
+    res.status(400).send(err);
+  }
 });
 
 module.exports = router;
