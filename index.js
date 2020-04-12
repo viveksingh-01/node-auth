758689c731a71e79eee79d7ac91a0987dd7080d7
viveksingh-01 <viveksingh9301@gmail.com>
2020-04-12 23:54:49 +0530

    Add DB connection

--- index.js
+++ index.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
 
+const mongoose = require('mongoose');
+
+// MongoDB connection
+mongoose.connect(
+  'mongodb+srv://vivek:nodeauth@cluster0-fbgxx.mongodb.net/test?retryWrites=true&w=majority',
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  () => {
+    console.log('Connected to DB');
+  }
+);
+
 // Import routes
 const authRoute = require('./routes/auth');
 
