a5eb5895dbe189618400c7dc4c4a2b2a98807caf
viveksingh-01 <viveksingh9301@gmail.com>
2020-04-14 13:58:46 +0530

    Add validation for existing user

--- routes/auth.js
+++ routes/auth.js
@@ -5,22 +5,29 @@ const { validateRegistration } = require('../validation');
 const User = require('../models/User');
 
 router.post('/register', async (req, res) => {
+  // Registration Data Validation
   const { error } = validateRegistration(req.body);
   if (error) {
-    res.status(400).send(error.details[0].message);
-  } else {
-    const user = new User({
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password,
-    });
+    return res.status(400).send(error.details[0].message);
+  }
 
-    try {
-      const savedUser = await user.save();
-      res.send(savedUser);
-    } catch (err) {
-      res.status(400).send(err);
-    }
+  // Check if the User already exists
+  const emailExists = await User.findOne({ email: req.body.email });
+  if (emailExists) {
+    return res.status(400).send('Email already exists!');
+  }
+
+  const user = new User({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password,
+  });
+
+  try {
+    const savedUser = await user.save();
+    res.send(savedUser);
+  } catch (err) {
+    res.status(400).send(err);
   }
 });
 
